2fe8e94a97bc118c8d60b3acc685877081e2c4be
unknown <alapottratrojon@gmail.com>
2024-02-10 01:05:46 +0600

    delete option done

--- src/app/_components/Todos.tsx
+++ src/app/_components/Todos.tsx
@@ -1,19 +1,35 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { api } from "~/trpc/react";
 import { RouterOutputs } from "~/trpc/shared";
 
 const Todos = ({ todos }: { todos: RouterOutputs["todos"]["getLatest"] }) => {
+  const router = useRouter();
+  const deleteTodo = api.todos.delete.useMutation({
+    onSuccess: (data) => {
+      console.log(data,'data')
+      router.refresh();
+    },
+  });
+
   return (
     <div className="w-full">
-      <h2 className="text-xl font-bold uppercase text-orange-500" >Task List</h2>
+      <h2 className="text-xl font-bold uppercase text-orange-500">Task List</h2>
 
       <div className="mt-3 space-y-3">
-      {todos.map((todo) => (
-        <div className="w-full bg-slate-300 p-2 cursor-pointer hover:bg-slate-400/50 transition rounded-md text-sm py-3 flex justify-between" key={todo.id}>
+        {todos.map((todo) => (
+          <div
+            className="relative flex w-full cursor-pointer justify-between rounded-md bg-slate-300 p-4 py-3 text-sm transition hover:bg-slate-400/50"
+            key={todo.id}
+          >
             <p>{todo.task}</p>
             <span className="text-xs text-zinc-400">{todo.progress}</span>
-        </div>
-      ))}
+            <span onClick={()=> deleteTodo.mutate(todo.id)} className="absolute left-1 top-0 text-xs font-bold text-red-600">
+              x
+            </span>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/server/api/routers/todos.ts
+++ src/server/api/routers/todos.ts
@@ -2,15 +2,22 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { z } from "zod";
 import { CreateTodoSchema } from "../schema/todos";
 import { todos } from "~/server/db/schema";
+import { eq } from "drizzle-orm";
 
 export const todosRouter = createTRPCRouter({
-
- create: publicProcedure.input(CreateTodoSchema).mutation(async({ctx,input}) => {
-    const res = await ctx.db.insert(todos).values(input).execute();
-    return res;
- }),
+  create: publicProcedure
+    .input(CreateTodoSchema)
+    .mutation(async ({ ctx, input }) => {
+      const res = await ctx.db.insert(todos).values(input).execute();
+      return res;
+    }),
+  delete: publicProcedure
+    .input(z.number())
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.delete(todos).where(eq(todos.id, input)).execute();
+      return input
+    }),
   getLatest: publicProcedure.query(({ ctx }) => {
-   return ctx.db.query.todos.findMany();
+    return ctx.db.query.todos.findMany();
   }),
-
 });
